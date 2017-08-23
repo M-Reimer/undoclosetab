4402c0a74bc8e3277c72755ae625e6e39c0b01d1
Manuel Reimer <manuel.reimer@gmx.de>
2017-08-23 20:17:38 +0200

    Modified options page to not need "Save" button. Needs further work.

--- options.js
+++ options.js
@@ -1,61 +1,32 @@
 const numberText = document.querySelector("#number_inputbox");
-const numberRange = document.querySelector("#number_rangebox");
-const saveButton = document.querySelector("#save_button");
 
 function numberChanged(e) {
-  if (e.target.id == 'number_inputbox') {
-    numberRange.value = e.target.value;
-  }
-  else if (e.target.id == 'number_rangebox') {
-    numberText.value = e.target.value;
-  }
-  else {
-    console.error(`Unknown changes in id:${e.target.id}`);
-  }
+  var showNumber = parseInt(numberText.value);
+  if (isNaN(showNumber) ||
+      showNumber > numberText.max ||
+      showNumber < numberText.min)
+    showNumber = 6;
+
+  browser.storage.local.set({
+    showNumber: showNumber
+  });
+
+  numberText.value = showNumber;
 }
 
 function init() {
   document.querySelector("#number_label").textContent = browser.i18n.getMessage("menuitem_number_label");
-
   numberText.title = browser.i18n.getMessage("numberText_title");
-  numberRange.max = browser.sessions.MAX_SESSION_RESULTS;
+  numberText.max = browser.sessions.MAX_SESSION_RESULTS;
   loadOptions();
   numberText.addEventListener("change", numberChanged);
-  numberRange.addEventListener("change", numberChanged);
-  saveButton.innerText = browser.i18n.getMessage("saveButton_label");
-
-  document.querySelector("form").addEventListener("submit", saveOptions);
-}
-
-function showOnSaveButton(message, cleanDelay = 0) {
-  const defaultLabel = browser.i18n.getMessage("saveButton_label");
-  saveButton.innerText = message;
-  if (cleanDelay > 0)
-    setTimeout(() => { showOnSaveButton(defaultLabel); }, cleanDelay);
-}
-
-function saveOptions(e) {
-  e.preventDefault();
-  browser.storage.local.set({
-    showNumber: numberText.value
-  }).then(() => {
-    const message = browser.i18n.getMessage("saveButton_saved_label");
-    showOnSaveButton(message, 1500);
-  }, error => {
-    const message = browser.i18n.getMessage("saveButton_error_label");
-    showOnSaveButton(message, 2000);
-    console.error(error);
-  }
-  );
 }
 
 function loadOptions() {
-  function setCurrentOptions(result) {
+  browser.storage.local.get("showNumber").then((result) => {
     numberText.value = result.showNumber || 6;
-    numberRange.value = result.showNumber || 6;
-  }
-
-  browser.storage.local.get("showNumber").then(setCurrentOptions, error => {
+  },
+  (error) => {
     console.log(error);
   });
 }
