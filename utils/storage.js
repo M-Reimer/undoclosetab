95b84faf79afdc813f1cbed8ca72ea619961a096
Manuel Reimer <manuel.reimer@gmx.de>
2022-05-27 15:18:45 +0200

    Add managed preference support

--- utils/storage.js
+++ utils/storage.js
@@ -32,7 +32,9 @@ const Storage = {
       json = json.replace(/__MSG_([A-Za-z0-9_]+?)__/g, (match, msgName) => {
         return browser.i18n.getMessage(msgName);
       });
-      this._defaults = JSON.parse(json);
+      const defaults = JSON.parse(json);
+      await this._apply_managed_defaults(defaults);
+      this._defaults = defaults;
     }
 
     const prefs = await browser.storage.local.get();
@@ -43,11 +45,64 @@ const Storage = {
     return prefs;
   },
 
+  _gettype: function(object) {
+    return Object.prototype.toString.call(object);
+  },
+
   set: function(aObject) {
     return browser.storage.local.set(aObject);
   },
 
   remove: function(keys) {
     return browser.storage.local.remove(keys);
+  },
+
+
+  // IMPORTANT!!!
+  // If you found this, then you hopefully know what you are doing!
+  // Mozilla decided against verification of managed preferences:
+  // https://bugzil.la/1230802#c30
+  // The following code does *very basic* verification on *very low* level,
+  // but you can still create unsupported cases very easily, which could make my
+  // Add-ons break. I have no regard for managed preferences! The way how I
+  // handle my preferences could change with any Add-on release, and it is
+  // *your job* to keep track of this if you decide to use managed preferences!
+  _apply_managed_defaults: async function(defaults) {
+    const mgdefaults = await browser.storage.managed.get();
+
+    // Run over all managed preference values that target an existing default
+    for (const [name, mgvalue] of Object.entries(mgdefaults)) {
+      if (!Object.hasOwn(defaults, name))
+        continue;
+      const ourvalue = defaults[name];
+
+      // Do not allow managed preferences to change a type of a preference
+      const mgtype = Object.prototype.toString.call(mgvalue);
+      const ourtype = Object.prototype.toString.call(ourvalue);
+      if (mgtype !== ourtype) {
+        console.log(`Managed default applying failed for "${name}" which is of type ${mgtype} but should be of type ${ourtype}`);
+        continue;
+      }
+
+      // Do not allow managed preferences to empty arrays
+      if (Array.isArray(mgvalue) && mgvalue.length == 0) {
+        console.log(`Managed default applying failed for "${name}" because managed preferences are not allowed to empty arrays`);
+        continue;
+      }
+
+      // Force managed array values to be of same type as our defaults
+      if (Array.isArray(ourvalue) && ourvalue.length > 0) {
+        const itemtype = Object.prototype.toString.call(ourvalue[0]);
+        const mgtypes = mgvalue.map(x => Object.prototype.toString.call(x));
+        const invalid = mgtypes.filter(x => x !== itemtype);
+        if (invalid.length) {
+          console.log(`Managed default applying failed for "${name}" because one of its items is of type ${invalid[0]} but should be of type ${itemtype}`);
+          continue;
+        }
+      }
+
+      // If we reach here, then we apply the managed default value
+      defaults[name] = mgvalue;
+    }
   }
 };
